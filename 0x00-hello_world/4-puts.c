02b6b9659b6cb07e77f43936a4bbbbcd88ee194a
Joseph Uche <ucheejike161@gmail.com>
2023-08-10 11:27:50 -0700

    Added the files 4-puts.c 5-printf.c 6-size.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * The main is the entry point of the program
+ * As long as the program returns 0 it's successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * The main is the entry point of the program
+ * If it returns 0 its successful
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	print("Size of a long int: %zu byte(s)\n" sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
