d5a0e7c9e3449ce2e7dbab24d6eff48be3bed6f8
Joseph Uche <ucheejike161@gmail.com>
2023-08-10 12:34:09 -0700

    Modified the file 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints "and that piece of art is useful" - Dora Kopar, 2015-10-19",
- * 		  followed by a new line, to standard error.
+ * followed by a new line, to standard error.
  *
  * Return: Always 1.
  */
