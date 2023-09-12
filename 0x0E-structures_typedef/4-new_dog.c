d4a510b5c79a9eb0d91bfea3f315a2a93a6e73eb
Joseph Uche <ucheejike161@gmail.com>
2023-09-11 19:57:57 -0700

    Modification

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * new_dog - creates a new dog.
@@ -9,44 +8,46 @@
  * @owner: owner of the dog.
  *
  * Return: struct dog.
- * If fails, returns NULL.
+ * if fails, returns NULL.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *p_dog;
-    int lname, lowner;
+	dog_t *p_dog;
+	int i, lname, lowner;
 
-    // Check for NULL pointers
-    if (!name || !owner)
-        return (NULL);
+	p_dog = malloc(sizeof(*p_dog));
+	if (p_dog == NULL || !(name) || !(owner))
+	{
+		free(p_dog);
+		return (NULL);
+	}
 
-    // Allocate memory for the dog structure
-    p_dog = malloc(sizeof(dog_t));
-    if (!p_dog)
-        return (NULL);
+	for (lname = 0; name[lname]; lname++)
+		;
 
-    // Calculate string lengths
-    lname = strlen(name);
-    lowner = strlen(owner);
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
 
-    // Allocate memory for name and owner strings
-    p_dog->name = malloc(lname + 1);
-    p_dog->owner = malloc(lowner + 1);
+	p_dog->name = malloc(lname + 1);
+	p_dog->owner = malloc(lowner + 1);
 
-    if (!p_dog->name || !p_dog->owner)
-    {
-        // Free allocated memory in case of an error
-        free(p_dog->owner);
-        free(p_dog->name);
-        free(p_dog);
-        return (NULL);
-    }
+	if (!(p_dog->name) || !(p_dog->owner))
+	{
+		free(p_dog->owner);
+		free(p_dog->name);
+		free(p_dog);
+		return (NULL);
+	}
 
-    // Copy name and owner strings
-    strcpy(p_dog->name, name);
-    strcpy(p_dog->owner, owner);
+	for (i = 0; i < lname; i++)
+		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
 
-    p_dog->age = age;
+	p_dog->age = age;
 
-    return (p_dog);
+	for (i = 0; i < lowner; i++)
+		p_dog->owner[i] = owner[i];
+	p_dog->owner[i] = '\0';
+
+	return (p_dog);
 }
